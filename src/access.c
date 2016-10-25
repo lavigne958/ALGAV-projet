6e1ef85d81939bfa121591bd196c758ea4646851
Hatoux <hatouxx@gmail.com>
2016-10-25 17:17:22 +0200

    supression des warnings

--- src/access.c
+++ src/access.c
@@ -11,7 +11,7 @@ node* creer_noeud(){
     node* retour;
     
     if( (retour = (node*) malloc(sizeof(node))) == NULL){
-        fprintf(stderr,"erreur malloc creer noeud\n");
+        fprintf(stderr,"erreur malloc dans creer_noeud\n");
         exit(EXIT_FAILURE);
     }
 
@@ -23,58 +23,57 @@ node* creer_noeud(){
 
 
 int is_node_null(node* n){
-    if(n == NULL){
-       fprintf(stderr,"erreur node = null\n");
-       return 1;
-    }else{
-        return 0;
-    }
+       return n == NULL;
 }
 
 int creer_fils(node* n){
     int i;
 
     if(is_node_null(n)){
-        return -1;
+      fprintf(stderr,"erreur node = null , creer fils\n");
+      return -1;
     }
 	
-	n->fils = (node**) malloc(sizeof(node*) * (NB_CHAR_MAX + 1));
-	
-	if(n->fils == NULL){
-		fprintf(stderr,"erreur node->fils = null , creer fils\n");
-		return -1;
-	}
-	
+    n->fils = (node**) malloc(sizeof(node*) * (NB_CHAR_MAX + 1));
+    
+    if(n->fils == NULL){
+      fprintf(stderr,"erreur node->fils = null , creer fils\n");
+      return -1;
+    }
+    
     for(i = 0; i < (NB_CHAR_MAX + 1); i++){
-        n->fils[i] = (node*) malloc(sizeof(node));
-        if(is_node_null(n->fils[i])){
-			return -1;
-		}
+      n->fils[i] = (node*) malloc(sizeof(node));
+      if(is_node_null(n->fils[i])){
+	return -1;
+      }
     }
     
     return 0;
 }
 
 char* get_prefix(node* n){
-	
+  
     if( is_node_null(n) ){
-        return -1;
+      return NULL;
     }
-	return n->prefix;
+    return n->prefix;
 }
 
 node* get_fils_node(node* n, char index){
-	if(is_node_null(n)){
-        return -1;
-    }
-	
-	node* fils = n->fils[(int)index];
-	
-	if(is_node_null(fils)){
-        return -1;
-	}
-	
-	return fils;
+
+  node* fils;
+
+  if(is_node_null(n)){
+    return NULL; 
+  }
+  
+  fils = n->fils[(int)index];
+  
+  if(is_node_null(fils)){
+    return NULL;
+  }
+  
+  return fils;
 }
-    
-    
+
+
