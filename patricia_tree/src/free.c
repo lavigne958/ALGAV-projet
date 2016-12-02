989b0cb2871e582b0b5c2a2c16f4d20dbc6cdfad
Hatoux <hatouxx@gmail.com>
2016-12-02 16:38:10 +0100

    free qui ne free rien du tout

--- patricia_tree/src/free.c
+++ patricia_tree/src/free.c
@@ -9,12 +9,12 @@ void free_tree(node* n){
 
   int i;
   
-  if(has_childs(n)){
+  if(has_childs(n) ){
     for(i = 0; i<NB_CHAR_MAX; i++){
-      if(n->tab_fils[i] != NULL){
-	free_tree(n->tab_fils[i]);
-      }
+      if(n->tab_fils[i] != NULL)
+	free_tree( n->tab_fils[i] );
     }
   }
   free(n);
+  
 }
