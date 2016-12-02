198336278eb00c8ff747258a9fb1a0665da32e85
Hatoux <hatouxx@gmail.com>
2016-12-02 16:40:18 +0100

    clone que je pensais avoir deja add

---
+++ patricia_tree/include/clone.h
@@ -0,0 +1,8 @@
+#ifndef _CLONE_H
+#define _CLONE_H
+
+node* clone(node* n);
+
+node** clone_tab_fils(node** tf);
+
+#endif
---
+++ patricia_tree/src/clone.c
@@ -0,0 +1,38 @@
+#include <string.h>
+
+#include "struct.h"
+#include "access.h"
+#include "util.h"
+
+node** clone_tab_fils(node** tf);
+
+node* clone(node* n){
+  node* new_node;
+  if(!n)
+    exit_failure("clone","le node a cloner n'existe pas");
+  
+  new_node = creer_noeud();
+  set_prefix(new_node, get_prefix(n));
+  if(has_childs(n)){
+    new_node->tab_fils = clone_tab_fils(n->tab_fils);
+  }
+
+  return new_node;
+}
+
+node** clone_tab_fils(node** tf){
+  node ** new_tf;
+  int i;
+  
+  if(!tf)
+    exit_failure("clone_tab_fils","le tab de node a cloner n'existe pas");
+
+  new_tf = creer_tab_sans_pere();
+  for(i = 0; i < NB_CHAR_MAX; i++){
+      if(tf[i] != NULL){
+        new_tf[i] = clone(tf[i]);
+      }
+  }
+  return new_tf;
+}
+
