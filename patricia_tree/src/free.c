532d3da7a57798371533f8f29cff8224a1e91ff2
Hatoux <hatouxx@gmail.com>
2016-12-02 12:56:26 +0100

    free_tree pour les patricia

---
+++ patricia_tree/src/free.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+#include "struct.h"
+#include "access.h"
+
+
+
+void free_tree(node* n){
+
+  int i;
+  
+  if(has_childs(n)){
+    for(i = 0; i<NB_CHAR_MAX; i++){
+      if(n->tab_fils[i] != NULL){
+	free_tree(n->tab_fils[i]);
+      }
+    }
+  }
+  free(n);
+}
